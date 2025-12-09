1008d1ccde2cf33a6bec206aa9fef5ea888294f1
Gabriel-Emannuel <gabriel.emannuel.gama.andrade@gmail.com>
2025-12-08 21:50:19 -0300

    feat: OCR manager v0

--- src/ocr/models.py
+++ src/ocr/models.py
@@ -1,4 +1,17 @@
-from typing import TypedDict
+from typing_extensions import TypedDict
+
+class OCRInput(TypedDict):
+    ocr_path: str
+    regex_question: str
+    regex_id: str
+
+class Answer(TypedDict):
+    question_id: str
+    answer: str
+
+class Correction(TypedDict):
+    student_id: str
+    answers: list[Answer]
 
 class OCRResult(TypedDict):
-    ocr: str
+    corrections: list[Correction]
